03cffbb10ecccd90a78976606caa40449451a1d0
Sergey <muh-s27@yandex.ru>
2022-06-01 09:16:42 +0300

    commit

--- test_t4/test_common_bases/test_1_login.py
+++ test_t4/test_common_bases/test_1_login.py
@@ -1,11 +1,11 @@
 from Base.Base_functions import wait_of_element_located, login_T4
 from Base.Dictionary_xpath import init_dict_xpath_login
-# import allure
+import allure
 
 # Функция авторизации в системе
-# @allure.feature('Базовое тестирование')
-# @allure.story('1) Авторизация в системе T4MP.v2')
-# @allure.severity('blocker')
+@allure.feature('Базовое тестирование')
+@allure.story('1) Авторизация в системе T4MP.v2')
+@allure.severity('blocker')
 def test_login(get_driver, get_init_data):
 # def test_login():
     """
